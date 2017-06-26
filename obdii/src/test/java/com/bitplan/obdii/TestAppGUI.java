699dd7e3cc81cc25ee65f9b8b37eb979a7455ff2
Wolfgang Fahl <wf@bitplan.com>
2017-06-26 11:25:38 +0200

    adds missing files

---
+++ obdii/src/test/java/com/bitplan/obdii/TestAppGUI.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * This file is part of the https://github.com/BITPlan/can4eve open source project
+ *
+ * Copyright 2017 BITPlan GmbH https://github.com/BITPlan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *  http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bitplan.obdii;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bitplan.can4eve.gui.App;
+
+public class TestAppGUI {
+  @Test
+  public void testAppGUI() throws Exception {
+    App app=App.getInstance();
+    assertNotNull(app);
+    assertEquals(2,app.getMainMenu().getSubMenus().size());
+    assertEquals(2,app.getForms().size());
+  }
+}
