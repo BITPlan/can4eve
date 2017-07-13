feedc973e7b615bd15db50af7f47bdcc16daab54
Wolfgang Fahl <wf@bitplan.com>
2017-07-13 07:01:58 +0200

    uses available in streamconnector

--- elm327/src/main/java/com/bitplan/elm327/StreamConnector.java
+++ elm327/src/main/java/com/bitplan/elm327/StreamConnector.java
@@ -41,120 +41,122 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-
 /**
  * connect the given input stream to the given output stream
  */
 public class StreamConnector extends Thread {
-    Log log;
-    public final static int BUFFER_SIZE = 4096;
-    protected boolean running = false;
-    String title;
+  Log log;
+  public final static int BUFFER_SIZE = 4096;
+  protected boolean running = false;
+  String title;
 
-    /**
+  /**
+   * 
+   * Input stream to read from.
+   */
+  private InputStream is = null;
 
-     * Input stream to read from.
-     */
-    private InputStream is = null;
+  /**
+   * Output stream to write to.
+   */
+  private OutputStream os = null;
 
-    /**
-     * Output stream to write to.
-     */
-    private OutputStream os = null;
+  public Log getLog() {
+    return log;
+  }
 
-    public Log getLog() {
-        return log;
-    }
+  public void setLog(Log log) {
+    this.log = log;
+  }
 
-    public void setLog(Log log) {
-        this.log = log;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  /**
+   * Specify the streams that this object will connect in the {@link #run()}
+   * method.
+   *
+   * @param is
+   *          the InputStream to read from.
+   * @param os
+   *          the OutputStream to write to.
+   */
+  public StreamConnector(InputStream is, OutputStream os) {
 
-    /**
-     * Specify the streams that this object will connect in the {@link #run()}
-     * method.
-     *
-     * @param is
-     *          the InputStream to read from.
-     * @param os
-     *          the OutputStream to write to.
-     */
-    public StreamConnector(InputStream is, OutputStream os) {
+    this.is = is;
+    this.os = os;
 
-        this.is = is;
-        this.os = os;
+  }
 
-    }
+  /**
+   * Connect the InputStream and OutputStream objects specified in the
+   * {@link #StreamConnector(InputStream, OutputStream)} constructor.
+   */
+  public void run() {
+    assert (is != null);
+    assert (os != null);
 
-    /**
-     * Connect the InputStream and OutputStream objects specified in the
-     * {@link #StreamConnector(InputStream, OutputStream)} constructor.
-     */
-    public void run() {
-        assert (is != null);
-        assert (os != null);
+    // If the InputStream or output stream is null, don't do anything
+    if ((is == null) || (os == null))
+      return;
 
-        // If the InputStream or output stream is null, don't do anything
-        if ((is == null) || (os == null))
-            return;
-
-        // Connect the streams for ever
-        running = true;
-        while (running) {
-            try {
-                int bytesRead = 0;
-                byte[] buf = new byte[BUFFER_SIZE];
-                while ((bytesRead = is.read(buf)) != -1) {
-                    // if one of the streams is vanished
-                    if (is==null)
-                        break;
-                    if (os==null)
-                        break;
-                    if (bytesRead > 0) {
-                        os.write(buf, 0, bytesRead);
-                        os.flush();
-                    }
-                    if (log!=null) {
-                        log.log("piped " + bytesRead + " bytes " + title);
-                        if (bytesRead<80) {
-                            String str = new String(buf,0,bytesRead, "UTF-8");
-                            log.log(str);
-                        }
-                    }
-                    // yield();
-                }
-            } catch (IOException e) {
-                if (running) {
-                    if (log!=null)
-                        log.handle("StreamConnector " + title + " issue ", e);
-                    // Stream closed might happen e.g. if USB connection is disconnected
-                    if ("Stream Closed".equals(e.getMessage())) {
-                        halt();
-                    }
-                }
+    // Connect the streams for ever
+    running = true;
+    while (running) {
+      try {
+        int bytesRead = 0;
+        int available = is.available();
+        if (available > 0) {
+          byte[] buf = new byte[available];
+          while ((bytesRead = is.read(buf)) != -1) {
+            // if one of the streams is vanished
+            if (is == null)
+              break;
+            if (os == null)
+              break;
+            if (bytesRead > 0) {
+              os.write(buf, 0, bytesRead);
+              os.flush();
             }
-        } // while
-        // we purposely left the while loop so we are not running any more
-        running=false;
-    }
-
-    /**
-     * stop this thread
-     */
-    public void halt() {
-        synchronized (this) {
-            is = null;
-            os = null;
-            running = false;
+            if (log != null) {
+              log.log("piped " + bytesRead + " bytes " + title);
+              if (bytesRead < 80) {
+                String str = new String(buf, 0, bytesRead, "UTF-8");
+                log.log(str);
+              }
+            }
+            // yield();
+          }
         }
+      } catch (IOException e) {
+        if (running) {
+          if (log != null)
+            log.handle("StreamConnector " + title + " issue ", e);
+          // Stream closed might happen e.g. if USB connection is disconnected
+          if ("Stream Closed".equals(e.getMessage())) {
+            halt();
+          }
+        }
+      }
+    } // while
+    // we purposely left the while loop so we are not running any more
+    running = false;
+  }
+
+  /**
+   * stop this thread
+   */
+  public void halt() {
+    synchronized (this) {
+      is = null;
+      os = null;
+      running = false;
     }
+  }
 
 }
